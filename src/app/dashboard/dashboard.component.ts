17ea5f7238aead0bfe922cdcd1e8b1c8f0adde56
jvila <julio.vila@raymondjames.com>
2018-07-08 10:40:24 -0400

    Format to show time from now in thumbnail subtitles

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import moment from 'moment';
 import {VideoService} from '../videos/video.service';
 
 @Component({
@@ -8,6 +9,7 @@ import {VideoService} from '../videos/video.service';
 })
 export class DashboardComponent implements OnInit {
   videos = [];
+  moment = moment;
   options = {
     order: 'relevance',
     q: '',
